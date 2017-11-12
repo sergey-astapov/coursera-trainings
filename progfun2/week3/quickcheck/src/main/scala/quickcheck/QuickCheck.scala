90658c5dac09cbe49e54799a5ffa1504cb9f8ac8
sergey-astapov <serg.astapov@gmail.com>
2017-11-12 12:39:52 +0300

    - week3 done

--- progfun2/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,15 +1,17 @@
 package quickcheck
 
-import common._
-
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen._
+import org.scalacheck.Prop._
 import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    a <- arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(a, h)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -17,4 +19,27 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("minOf2") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(insert(a - 1, h)) == a - 1
+  }
+
+  property("mergeEmpty") = forAll { h: H =>
+    meld(h, empty) == h
+  }
+
+  property("deleteMin") = forAll { (h: H) =>
+    def _del(h: H, list: List[Int]): List[Int] = if (isEmpty(h)) list else {
+      val m = findMin(h)
+      _del(deleteMin(h), list ++ List(m))
+    }
+
+    val list = _del(h, Nil)
+    list == list.sorted
+  }
 }
