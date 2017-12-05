8edb867591b643a88d8ab9c165d28f39171885dc
sergey-astapov <serg.astapov@gmail.com>
2017-12-06 00:50:42 +0300

    - intermediate

--- parprog1/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ parprog1/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -45,10 +45,7 @@ object ParallelCountChange {
   /** Returns the number of ways change can be made from the specified list of
    *  coins for the specified amount of money.
    */
-  def countChange(money: Int, coins: List[Int]): Int =
-    seqCountChange(money, coins)
-
-  def seqCountChange(money: Int, coins: List[Int]): Int = {
+  def countChange(money: Int, coins: List[Int]): Int = {
     val sortedCoins = coins.sorted.toArray
 
     def howMany(m: Int, i: Int): Int = (m, i) match {
@@ -65,26 +62,53 @@ object ParallelCountChange {
     howMany(money, sortedCoins.length - 1)
   }
 
+//  def countChange(money: Int, coins: List[Int]): Int = parCountChange(money, coins, (_, _) => false)
+
   type Threshold = (Int, List[Int]) => Boolean
+
+  sealed trait Tree
+
+  final case class Node(ls: Tree, rs: Tree) extends Tree
+
+  final case class Leaf(count: Int) extends Tree
 
   /** In parallel, counts the number of ways change can be made from the
    *  specified list of coins for the specified amount of money.
    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    ???
+    if (threshold(money, coins)) countChange(money, coins)
+    else {
+      val sortedCoins = coins.sorted.toArray
+
+      def howMany(m: Int, i: Int): Tree = (m, i) match {
+        case (x, _) if x < 0 => Leaf(0)
+        case (x, _) if x == 0 => Leaf(1)
+        case (_, index) if index < 0 => Leaf(0)
+        case (x, index) if x < sortedCoins(index) => howMany(x, index - 1)
+        case (x, index) if x == sortedCoins(index) => Node(Leaf(1), howMany(x, index - 1))
+        case (x, index) =>
+          val r = x - sortedCoins(index)
+          Node(howMany(r, index), howMany(x, index - 1))
+      }
+
+      def traverse(tree: Tree): Int = tree match {
+        case Node(ls, rs) => traverse(ls) + traverse(rs)
+        case Leaf(c) => c
+      }
+
+      traverse(howMany(money, sortedCoins.length - 1))
+    }
   }
 
   /** Threshold heuristic based on the starting money. */
-  def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+  def moneyThreshold(startingMoney: Int): Threshold = (x, _) => x <= 2 * startingMoney / 3
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
+    (_, coins) => coins.size <= 2 * totalCoins / 3
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
-  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ???
-  }
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold =
+    (x, coins) => x * coins.size <= 2 * startingMoney * allCoins.size / 3
 }
